666e46a57e9aaf0219631c269690f60c35a91c00
cindycarver <45807228+cindycarver@users.noreply.github.com>
2018-12-26 15:29:11 +0700

    change statuscode (#8)

--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/throw', function(req, res, next) {
-  res.send('respond with a resource');
+  res.sendStatus(200).end();
 });
 
 router.get('/burn', function(req, res, next) {
+  res.sendStatus(403).end();
+});
+
+router.get('/drown', function(req, res, next) {
   res.send('respond with a resource');
 });
 
