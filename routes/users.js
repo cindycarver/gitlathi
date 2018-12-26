7bfb82a0598ea993678de3c31b0755cc55731689
cindycarver <45807228+cindycarver@users.noreply.github.com>
2018-12-26 15:06:11 +0700

    dg redis, add burn (#6)

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,8 @@ router.get('/throw', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/burn', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 module.exports = router;
