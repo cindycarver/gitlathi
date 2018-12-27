7a55937d595fa07dbfed4f59cdafc83dd773b196
cindycarver <45807228+cindycarver@users.noreply.github.com>
2018-12-27 11:53:59 +0700

    direct (#13)

--- routes/catalogue.js
+++ routes/catalogue.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
- 
 
+
+router.get('/direct', function(req, res, next) {
+    res.sendStatus(200).end();
+});
 module.exports = router;
