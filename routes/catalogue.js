58a1d877785c198facae1fd1e07002cb497f0e1d
cindycarver <45807228+cindycarver@users.noreply.github.com>
2018-12-27 12:15:39 +0700

    f & g (#14)

--- routes/catalogue.js
+++ routes/catalogue.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/direct', function(req, res, next) {
+router.get('/fender', function(req, res, next) {
+    res.sendStatus(200).end();
+});
+
+router.get('/gibson', function(req, res, next) {
     res.sendStatus(200).end();
 });
 module.exports = router;
